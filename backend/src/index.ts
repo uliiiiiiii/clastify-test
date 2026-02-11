d57c05284d8f1486ef878f26df677da23fb0e7ce
Uli <ulilike161@gmail.com>
2026-02-11 12:46:04 +0100

    changed env variable name

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.MONGO_URI as string)
+  .connect(process.env.MONGODB_URI as string)
   .then(() => {
     console.log("✅ MongoDB connected");
     app.listen(PORT, () => {
